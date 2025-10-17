e4b5519f3875628407f2cfd38af58a93b52d717c
AmyN226 <anguyen226@student.hbuhsd.edu>
2025-10-17 21:46:20 +0000

    started

--- Main.java
+++ Main.java
@@ -4,6 +4,42 @@ public class Main
 {
   public static void main(String[] args)
   {
-    // write solutions to problems here
+    Scanner scan = new Scanner(System.in);
+    System.out.println("enter numbers to add. Enter -1 to stop");
+
+    int N = 0;
+    int sum = 0;
+    while (N != -1)
+    {
+        N = scan.nextInt();
+        sum += N;
+    }
+    sum++;
+    System.out.println("The sum is: " + sum);
+    
+
+    //problem 2
+    
+    System.out.println("enter numbers to find max. enter -1 to stop");
+
+    int currentNum = 0;
+    int max = 0;
+  
+    while (N != -1)
+    {
+      currentNum = scan.nextInt();
+        if (currentNum > max)
+        {
+          max = currentNum;
+
+        }
+        System.out.println("The max is: " + max);
+    }
+
+
+
+    
+    
+   scan.close();
   }
 }
